3db73f1d3b8eb9c98f8f9ca0664a4a5b7bc6fec1
saravanakumar3 <105409981+saravanakumar3@users.noreply.github.com>
2022-07-15 15:11:10 +0530

    Delete firstWebsitei directory

--- firstWebsitei/src/main/java/com/chainsys/miniproject/commonutil/InvalidInputDataException.java
+++
@@ -1,9 +0,0 @@
-package com.chainsys.miniproject.commonutil;
-
-public class InvalidInputDataException extends Exception {
-	public InvalidInputDataException() {
-		super("The Input Data is not Volid") ;
-	}public InvalidInputDataException(String message){
-		super(message);
-	}
-}
